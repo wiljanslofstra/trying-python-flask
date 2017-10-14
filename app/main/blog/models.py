a74fb359c9e8e17ed308c685b1b800bf7d3edff0
Wiljan Slofstra <wiljan@orangetalent.nl>
2017-10-14 20:56:15 +0200

    Add author table and relation to blogs

--- app/main/blog/models.py
+++ app/main/blog/models.py
@@ -7,3 +7,10 @@ class Blog(db.Model):
     slug = db.Column(db.String)
     content = db.Column(db.Text)
     published_at = db.Column(db.DateTime)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+    author = db.relationship('Author')
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
--- app/main/core/routes.py
+++ app/main/core/routes.py
@@ -1,16 +1,7 @@
 from ..main import app, db
 from ..blog import routes
-from flask import render_template
+from flask import render_template, redirect
 
 @app.route("/")
 def index():
-    result = db.engine.execute("SELECT * FROM blog")
-
-    blogs = []
-
-    for blog in result:
-        blogs.append(blog)
-
-    return ''.join(blogs)
-
-    return render_template('index.html')
+    return redirect('/blog')
