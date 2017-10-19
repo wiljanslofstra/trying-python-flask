48925a7fa1c3b9dc3cc890a28d818ec66beec467
Wiljan Slofstra <wiljan@orangetalent.nl>
2017-10-19 21:29:38 +0200

    Add up-to-date SQL dump, should add migrations though :)

---
+++ sql-dumps/app_19-10-2017_21-29.sql
@@ -0,0 +1,157 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4529
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.19)
+# Database: app
+# Generation Time: 2017-10-19 19:29:19 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table authors
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `authors`;
+
+CREATE TABLE `authors` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+LOCK TABLES `authors` WRITE;
+/*!40000 ALTER TABLE `authors` DISABLE KEYS */;
+
+INSERT INTO `authors` (`id`, `name`)
+VALUES
+	(1,'Wiljan Slofstra');
+
+/*!40000 ALTER TABLE `authors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table blogs
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `blogs`;
+
+CREATE TABLE `blogs` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `author_id` int(11) DEFAULT NULL,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `slug` varchar(255) NOT NULL DEFAULT '',
+  `content` text NOT NULL,
+  `published_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+LOCK TABLES `blogs` WRITE;
+/*!40000 ALTER TABLE `blogs` DISABLE KEYS */;
+
+INSERT INTO `blogs` (`id`, `author_id`, `title`, `slug`, `content`, `published_at`)
+VALUES
+	(1,1,'test123','test123','<p>\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sollicitudin eros vitae velit pharetra finibus. \n</p>\n\n<p>\nQuisque in imperdiet purus, ornare lobortis arcu. Mauris dignissim eleifend nisi, eu sagittis ex lacinia in. Ut quis odio et tortor tristique semper. Suspendisse ac tortor in arcu luctus facilisis quis ac nisi. Aliquam posuere volutpat dictum. Praesent id lectus et erat tristique dictum id pretium enim.\n</p>','2017-10-14 00:00:00'),
+	(2,1,'test1','test1','<p>\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sollicitudin eros vitae velit pharetra finibus. \n</p>\n\n<p>\nQuisque in imperdiet purus, ornare lobortis arcu. Mauris dignissim eleifend nisi, eu sagittis ex lacinia in. Ut quis odio et tortor tristique semper. Suspendisse ac tortor in arcu luctus facilisis quis ac nisi. Aliquam posuere volutpat dictum. Praesent id lectus et erat tristique dictum id pretium enim.\n</p>','2017-10-13 00:00:00'),
+	(3,1,'test2','test2','<p>Test</p>','2017-10-15 00:00:00');
+
+/*!40000 ALTER TABLE `blogs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table categories
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `categories`;
+
+CREATE TABLE `categories` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `slug` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+
+INSERT INTO `categories` (`id`, `title`, `slug`)
+VALUES
+	(1,'test','test'),
+	(2,'another category','another-category');
+
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table categories_blogs_association
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `categories_blogs_association`;
+
+CREATE TABLE `categories_blogs_association` (
+  `blogs_id` int(11) unsigned NOT NULL,
+  `categories_id` int(11) unsigned NOT NULL,
+  KEY `blogs_id` (`blogs_id`),
+  KEY `categories_id` (`categories_id`),
+  CONSTRAINT `blogs_id` FOREIGN KEY (`blogs_id`) REFERENCES `blogs` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `categories_id` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+LOCK TABLES `categories_blogs_association` WRITE;
+/*!40000 ALTER TABLE `categories_blogs_association` DISABLE KEYS */;
+
+INSERT INTO `categories_blogs_association` (`blogs_id`, `categories_id`)
+VALUES
+	(2,1),
+	(3,1),
+	(2,2),
+	(3,2),
+	(1,2);
+
+/*!40000 ALTER TABLE `categories_blogs_association` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table forms
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `forms`;
+
+CREATE TABLE `forms` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `email` varchar(255) NOT NULL DEFAULT '',
+  `message` text NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+LOCK TABLES `forms` WRITE;
+/*!40000 ALTER TABLE `forms` DISABLE KEYS */;
+
+INSERT INTO `forms` (`id`, `name`, `email`, `message`, `created_at`)
+VALUES
+	(3,'Test Test','wiljan@orangetalent.nl','test','2017-10-14 19:46:37');
+
+/*!40000 ALTER TABLE `forms` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
